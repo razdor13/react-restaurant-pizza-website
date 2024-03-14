df7d171ea1efb4a7146d1aa98261167c16aee7e4
Razdor13 <oneamaday@gmail.com>
2024-03-14 14:12:40 +0200

    added full first page static

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,22 @@
+import "../Categories/Categories.scss"
+
+
+
+
+const Categories = () => {
+    return (
+                <div class="categories">
+                    <ul>
+                        <li class="active">Все</li>
+                        <li>Мясные</li>
+                        <li>Вегетарианская</li>
+                        <li>Гриль</li>
+                        <li>Острые</li>
+                        <li>Закрытые</li>
+                    </ul>
+                </div>
+        
+    )
+}
+
+export default Categories
