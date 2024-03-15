8c574b102eee1298a856a8fa54c8dbf5655f51dc
Razdor13 <oneamaday@gmail.com>
2024-03-15 17:49:37 +0200

    made a small rework in file structure and used dynamic render + made btn selection functionality

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
 import "../Categories/Categories.scss"
 
 
 
 
 const Categories = () => {
-    return (
-                <div class="categories">
+    const [activeIndex ,setActiveIndex] = useState(0)
+
+    const categories = ['Всі',`М'ясні`,  'Овочеві' ,'Гриль' ,'Гострі']
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+     return (
+                <div className="categories">
                     <ul>
-                        <li class="active">Все</li>
-                        <li>Мясные</li>
-                        <li>Вегетарианская</li>
-                        <li>Гриль</li>
-                        <li>Острые</li>
-                        <li>Закрытые</li>
+                        {/* <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>Всі</li>
+                        <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? 'active' : ''}>М'ясні</li>
+                        <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? 'active' : ''}>Овочеві</li>
+                        <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? 'active' : ''}>Гриль</li>
+                        <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? 'active' : ''}> </li> */}
+                        {
+                            categories.map((categ,i) => 
+                            (<li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>{categ}</li>)
+                            )
+                        }
                     </ul>
                 </div>
         
